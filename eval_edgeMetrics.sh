abe2418c10ac839a5b8dec3041895b4655b19dc8
adossantos21 <adossantos21@outlook.com>
2025-09-05 13:31:21 -0500

    Updated eval_edgeMetrics.sh

--- eval_edgeMetrics.sh
+++ eval_edgeMetrics.sh
@@ -1,3 +1,4 @@
+# On the following line, `cityscapes-raw-eval` was formerly `python scripts/cityscapes_raw.py`
 cityscapes-raw-eval \
 /home/robert.breslin/datasets/cityscapes \
 /home/robert.breslin/alessandro/paper_2/mmsegmentation/work_dirs/sebnet_baseline-d-multilabel-head_1xb6_cityscapes/20250826_134843/SBD_preds/d_module \
