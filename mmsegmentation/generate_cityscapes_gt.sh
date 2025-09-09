44418f2d8040075dd75368c65f8a075d5e90e150
adossantos21 <adossantos21@outlook.com>
2025-09-09 08:00:36 -0500

    Added files to test and generate HED labels

--- mmsegmentation/generate_cityscapes_gt.sh
+++ mmsegmentation/generate_cityscapes_gt.sh
@@ -6,3 +6,13 @@ generate-cityscapes-gt \
 --nproc 8 \
 --root /home/robert.breslin/datasets/cityscapes/ \
 -o ../cityscapes_edges/ # This path is relative to the root path on the previous line
+
+python generate_hed_gt.py \
+--val_dir /home/robert.breslin/datasets/cityscapes_edges/gtEval/val/ \
+--input_suffix _gtProc_raw_edge.png \
+--output_suffix _gtProc_raw_hed_edge.png
+
+python generate_hed_gt.py \
+--val_dir /home/robert.breslin/datasets/cityscapes_edges/gtEval/val/ \
+--input_suffix _gtProc_thin_edge.png \
+--output_suffix _gtProc_thin_hed_edge.png
