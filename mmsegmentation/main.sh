01a4de02693e7c30b2f88383d9e552883cf1bec1
adossantos21 <adossantos21@outlook.com>
2025-09-16 16:30:51 -0500

    Updated shell files that launch trainings.

--- mmsegmentation/main.sh
+++ mmsegmentation/main.sh
@@ -1,6 +1,6 @@
 unset DISPLAY
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=2
 
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
-python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_1xb6_mapillaryv2.py
-#python tools/train.py configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_1xb6_mapillaryv2.py
+#python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py
+python tools/train.py configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_2xb6_mapillaryv2.py
--- mmsegmentation/main_dist.sh
+++ mmsegmentation/main_dist.sh
@@ -1,9 +1,11 @@
-unset DISPLAY
-export CUDA_VISIBLE_DEVICES=0,1
-export PORT=29500
+#unset DISPLAY
+#export CUDA_VISIBLE_DEVICES=2,3
+#export PORT=29505
+#export TORCH_DISTRIBUTED_DEBUG=DETAIL
+#./tools/dist_train.sh \
+#    configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_2xb6_mapillaryv2.py \
+#    2
 
-./tools/dist_train.sh \
-    configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py \
-    2
-#configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
+#configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py
 #configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_2xb6_mapillaryv2.py
+#configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
