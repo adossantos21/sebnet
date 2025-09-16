5adc90f151c8795c9cd9cb6477686a8cca74c39d
adossantos21 <alessandro.dossantos@ndus.edu>
2025-09-15 20:31:38 -0500

    Added files for computing class weights that address per pixel class imbalance

---
+++ compute_dataset_class_weights_cpp.sh
@@ -0,0 +1,7 @@
+g++ -std=c++17 compute_dataset_class_weights.cpp -o compute_weights `pkg-config --cflags --libs opencv4`
+
+./compute_weights \
+    --use_custom_path \
+    --num_classes 124 \
+    --dataset_dir '/home/robert.breslin/datasets/mapillary_vistas/training/v2.0/labels' \
+    --suffix '.png'
