df7a89c73aba77b6f51947f9dee0727344026c5a
adossantos21 <alessandro.dossantos@ndus.edu>
2025-09-15 20:30:12 -0500

    Renamed mapillary config files for different learning rate tests

--- mmsegmentation/configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_lr-5e-3_1xb6_mapillaryv2.py
+++ mmsegmentation/configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_lr-5e-3_1xb6_mapillaryv2.py
@@ -2,6 +2,11 @@ _base_ = [
     '../_base_/datasets/mapillary_v2.py',
     '../_base_/default_runtime.py'
 ]
+class_weight = [
+    0.8373, 0.918, 0.866, 1.0345, 1.0166, 0.9969, 0.9754, 1.0489, 0.8786,
+    1.0023, 0.9539, 0.9843, 1.1116, 0.9037, 1.0865, 1.0955, 1.0865, 1.1529,
+    1.0507
+]
 checkpoint_file = "/home/robert.breslin/alessandro/paper_2/mmpretrain/checkpoints/epoch_98.pth"
 data_root = '/home/robert.breslin/datasets/mapillary_vistas/'
 
@@ -36,7 +41,7 @@ model = dict(
         out_channels=256, 
         num_scales=5),    # The type of the neck module.
     decode_head=dict(
-        type='ConditionalBaselinePSBDBASHead',     # The type of the classification head module.
+        type='BaselinePDSBDBASHead',     # The type of the classification head module.
         # All fields except `type` come from the __init__ method of class `LinearClsHead`
         # and you can find them from https://mmpretrain.readthedocs.io/en/latest/api/generated/mmpretrain.models.heads.LinearClsHead.html
         num_classes=124,
@@ -48,14 +53,20 @@ model = dict(
                 type='OhemCrossEntropy',
                 thres=0.9,
                 min_kept=131072,
+                class_weight=class_weight,
                 loss_weight=1.0,
                 loss_name='loss_seg'),
             dict(
                 type='OhemCrossEntropy',
                 thres=0.9,
                 min_kept=131072,
+                class_weight=class_weight,
                 loss_weight=0.4,
                 loss_name='loss_seg_p'),
+            dict(
+                type='BoundaryLoss', 
+                loss_weight=20.0,
+                loss_name='loss_bd'),
             dict(
                 type='MultiLabelEdgeLoss',
                 loss_weight=5.0,
@@ -64,6 +75,7 @@ model = dict(
                 type='OhemCrossEntropy',
                 thres=0.9,
                 min_kept=131072,
+                class_weight=class_weight,
                 loss_weight=1.0,
                 loss_name='loss_bas')
         ]),
@@ -81,6 +93,7 @@ train_pipeline = [
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
+    dict(type='GenerateEdge', edge_width=4),
     dict(type='Mask2Edge', labelIds=list(range(0,124)), radius=2), # 0-19 for cityscapes classes
     dict(type='PackSegInputs')
 ]
@@ -88,13 +101,13 @@ train_dataloader = dict(batch_size=6, dataset=dict(data_root=data_root, pipeline
 val_dataloader = dict(batch_size=1, dataset=dict(data_root=data_root))
 test_dataloader = val_dataloader
 
-iters = 480000
+iters = 1452000
 val_interval=1000
 
 optim_wrapper = dict(
     # Use SGD optimizer to optimize parameters.
     type='mmpretrain.GradTrackingOptimWrapper',
-    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, 
+    optimizer=dict(type='SGD', lr=0.005, momentum=0.9, 
                    weight_decay=0.0005), clip_grad=None)
 
 # The tuning strategy of the learning rate.
