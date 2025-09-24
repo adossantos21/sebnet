c0162245ff3905ea30c2f5d6d7a81ff7b1667a15
adossantos21 <alessandro.dossantos@ndus.edu>
2025-09-24 14:41:56 -0500

    Updated main.sh and main_dist.sh

--- mmsegmentation/main.sh
+++ mmsegmentation/main.sh
@@ -3,6 +3,6 @@ export CUDA_VISIBLE_DEVICES=1
 
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py
-python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_1xb16_mapillaryv2.py
+#python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_1xb16_mapillaryv2.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb8_mapillaryv2.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_2xb6_mapillaryv2.py
