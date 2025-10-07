9229cfb9cabba7646fed74bfa3692ee5345deb58
adossantos21 <alessandro.dossantos@ndus.edu>
2025-10-07 16:46:50 -0500

    Minor changes for testing

--- mmsegmentation/fps.sh
+++ mmsegmentation/fps.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 python tools/analysis_tools/benchmark.py \
-    configs/pidnet/pidnet-l_2xb6-120k_1024x1024-cityscapes.py \
+    configs/sebnet/sebnet_ablation51_1xb6-160k_cityscapes.py \
     /home/robert.breslin/alessandro/paper_2/mmpretrain/work_dirs/pidnet-imagenet/20250702_154411/checkpoints/pidnet-imagenet/20250702_154411/epoch_100.pth \
     --repeat 200
--- mmsegmentation/main.sh
+++ mmsegmentation/main.sh
@@ -1,6 +1,12 @@
 unset DISPLAY
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=3
 
+#python tools/train.py configs/sebnet/sebnet_ablation51_1xb6-160k_cityscapes.py
+
+#python tools/train.py configs/sebnet/sebnet_ablation03_1xb6-160k_cityscapes.py
+#python tools/train.py configs/sebnet/sebnet_ablation04_1xb6-160k_cityscapes.py
+#python tools/train.py configs/sebnet/sebnet_ablation05_1xb6-160k_cityscapes.py
+python tools/train.py configs/sebnet/sebnet_ablation41_1xb6-160k_cityscapes.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py
 #python tools/train.py configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_1xb16_mapillaryv2.py
