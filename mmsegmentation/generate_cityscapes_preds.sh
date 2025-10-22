93061c601fe833376b7880734c19fd757fb7d66a
adossantos21 <adossantos21@outlook.com>
2025-10-21 21:38:11 -0500

    Updated generate_cityscapes_preds.sh

--- mmsegmentation/generate_cityscapes_preds.sh
+++ mmsegmentation/generate_cityscapes_preds.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=3
 python tools/analysis_tools/generate_edge_predictions.py \
-    configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py \
-    ../../../paper_2/mmsegmentation/SBD/ablation25/ckpt/160K/best_mIoU.pth \
-    SBD/ablation25/preds/160K/
+    configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_1xb6_cityscapes.py \
+    /home/robert.breslin/alessandro/testing/paper_2/mmsegmentation/work_dirs/sebnet_ablation33_1xb6-160k_cityscapes/20250906_102650/checkpoints/best_mIoU.pth \
+    SBD/table5Best/preds/240K/
