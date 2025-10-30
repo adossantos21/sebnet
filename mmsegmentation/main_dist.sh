f072ea1ce21a8d1057362040a7274260fbb52815
adossantos21 <alessandro.dossantos@ndus.edu>
2025-10-30 09:32:08 -0500

    Minor updates

--- mmsegmentation/main_dist.sh
+++ mmsegmentation/main_dist.sh
@@ -5,8 +5,3 @@ export GPUS=4
 ./tools/dist_train.sh \
     configs/sebnet/sebnet_ablation49_1xb6-160k_mapillary.py \
     $GPUS
-
-#configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb6_mapillaryv2.py
-#configs/sebnet/sebnet_baseline-p-d-sbd-bas-head_2xb8_mapillaryv2.py
-#configs/sebnet/sebnet_baseline-p-sbd-bas-head-conditioned_2xb6_mapillaryv2.py
-#configs/sebnet/sebnet_baseline-p-d-bas-head_1xb6_cityscapes.py
