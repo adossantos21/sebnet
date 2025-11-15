a77d5a930c1b4d49105dc32e7032a1774daa103d
Alessandro Dos Santos <91921962+adossantos21@users.noreply.github.com>
2025-11-14 23:27:37 -0600

    Update dataset directory path in script

--- tools/compute_dataset_class_weights_py.sh
+++ tools/compute_dataset_class_weights_py.sh
@@ -1,6 +1,6 @@
 python compute_dataset_class_weights.py \
     --use_custom_path \
     --num_classes 11 \
-    --dataset_dir '/home/robert.breslin/datasets/CamVid/train_labels/*.png' \
+    --dataset_dir 'data/CamVid/train_labels/*.png' \
     --ignore_index 255 \
     --cam_vid \
